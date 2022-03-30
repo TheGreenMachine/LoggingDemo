fe903d6a08665ff2cd1d631a9af4340bc7f8c52b
David Day <dcday@themilwaukeeroad.com>
2022-03-29 20:34:48 -0500

    update to only pull latest

--- RobotLogs.ps1
+++ RobotLogs.ps1
@@ -1,10 +1,13 @@
 $rio = 'rio'  # this is IP address of roborio
 $logdir = "D:\Util\logs"  # update this path to the directory where you placed badlogvis.exe
 $rioFound = Test-Connection $rio -Count 1 -TTL 2 -Quiet
+
 if ($rioFound) {
-    Write-Host -ForegroundColor Cyan "Copying logs from roboRio"
-    & scp -p $rio`:/home/lvuser/*.bag "$($logdir)\"
-    & scp -p $rio`:/media/sda1/*.bag "$($logdir)\"
+    Write-Host -ForegroundColor Cyan "Copying most recent from roboRio"
+    Start-Process -FilePath ssh -ArgumentList "ls -rt /home/lvuser/*.bag | tail -1" -Wait -WindowStyle Hidden -RedirectStandardOutput "$($logdir)\recent.log"
+    $recent = Get-Content "$($logdir)\recent.log"
+    & scp -p $rio`:/home/lvuser/$recent "$($logdir)\"
+    & scp -p $rio`:/media/sda1/$recent "$($logdir)\"
     Write-Host -ForegroundColor Yellow "Removing logs from roboRio"
     Start-Process -FilePath ssh -ArgumentList "$rio rm *.bag" -Wait -WindowStyle Hidden  #remove files from rio to prevent disk space issues
     Start-Process -FilePath ssh -ArgumentList "$rio rm /media/sda1/*.bag" -Wait -WindowStyle Hidden  #remove files from rio to prevent disk space issues
