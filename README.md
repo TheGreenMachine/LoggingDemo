338f7b43113ef6be8fde05c9de39408a9377d925
David Day <dcday@themilwaukeeroad.com>
2022-08-17 21:18:57 -0500

    update script to run as import or cmd line. Update readme Add vs code launch Update to 2022

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 Example FRC program to demo advanced logging
---------------------------------------------
+============================================
 
-Project uses https://github.com/dominikWin/badlog for logging in java and https://github.com/dominikWin/badlogvis to
+Project uses <https://github.com/dominikWin/badlog> for logging in java and <https://github.com/dominikWin/badlogvis> to
 generate the html.
 
-This requires a pigeon from cross the road electronics and one TalonSRX to run unmodified.
-There is a powershell script to automate pulling files from the robot and process them. So you need Windows,
-and it uses ssh to access robo rio
- 
-* To get started download html generated from https://github.com/dominikWin/badlogvis/releases/latest
+To run this program you will need a pigeon from cross the road electronics and one TalonSRX to run unmodified.
+There is a powershell script to automate pulling files from the robot and process them. So you need Windows with Powershell 7 or newer,
+and it uses ssh to access robo rio.
+
+* To get started download html generator from <https://github.com/dominikWin/badlogvis/releases/latest>
 and put in a local folder.
 * Update code to use your can ids or comment out code will rotate one motor based on gyro so elevate
 * Deploy code to your robot
--- RobotLogs.ps1
+++ RobotLogs.ps1
@@ -1,3 +1,5 @@
+#Requires -PSEdition Core
+
 <#
 .SYNOPSIS
 Gets Logs from robot
@@ -6,10 +8,10 @@ Gets Logs from robot
 Gets logs from roborio in lvusr home or usb storage or from simulator
 
 .PARAMETER rio
-ssh address of rio use ip address or host
+ssh address of rio use ip address or host. Change 'rio' to your ip
 
 .PARAMETER logdir
-directory that holds your output logs and badlogvis.exe
+directory that holds your output logs and badlogvis.exe. Change "D:\Util\logs" to you path where badlogvis is located.
 
 .PARAMETER last
 how many of the most recent files to retrieve
@@ -17,8 +19,9 @@ how many of the most recent files to retrieve
 #>
 function GetRobotLogs( $rio = 'rio', $logdir = "D:\Util\logs", $last = 1 ) {
     $rioFound = Test-Connection $rio -Count 1 -TTL 2 -Quiet
+    Write-Host -ForegroundColor Cyan "Checking for connection to host: $rio"
     if ($rioFound) {
-        Write-Host -ForegroundColor Cyan "Copying most recent from roboRio"
+        Write-Host -ForegroundColor Cyan "Copying most recent from roboRio to $logdir"
         $recent = & ssh -fq $rio ls -rt /home/lvuser/*.bag | tail -$($last)
         if($recent) {
             & scp -p $rio`:$recent "$($logdir)\"
@@ -27,11 +30,11 @@ function GetRobotLogs( $rio = 'rio', $logdir = "D:\Util\logs", $last = 1 ) {
             & ssh -fq $rio rm /media/sda1/*.bag #remove files from rio to prevent disk space issues
         }
     }
-    Write-Host -ForegroundColor Cyan "Copying simulation logs"
+    Write-Host -ForegroundColor Cyan "Copying simulation logs to $logdir"
     Get-ChildItem $env:TEMP\*.bag | Sort-Object -Descending -Property LastWriteTime -Top $last | Move-Item -Destination $logdir -Force
     $bagFiles = Get-ChildItem $logdir\*.bag
     if ($bagFiles) {
-        Write-Host -ForegroundColor Cyan "Creating html files"
+        Write-Host -ForegroundColor Cyan "Creating html files in $logdir"
         $bagFiles | ForEach-Object { & "$logdir\badlogvis.exe" $_ }
         Remove-Item $logdir\*.bag *> $null
         & $(Get-ChildItem $logdir\*.html | Sort-Object -Descending -Property LastWriteTime -Top 1)
@@ -41,6 +44,7 @@ function GetRobotLogs( $rio = 'rio', $logdir = "D:\Util\logs", $last = 1 ) {
     }
 }
 
-if ($null -ne $psEditor) {
+# Do not execute if being used as a include
+if ("" -eq $MyInvocation.ScriptName) {
     GetRobotLogs
 }
